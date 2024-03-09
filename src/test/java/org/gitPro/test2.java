10613e1d013b0e6ff0f01b7b252eb2b6564bfac7
kishanth <kishanth@LAPTOP-AGR3S4NJ>
2024-03-10 05:00:33 +0530

    committing test2 o branch x

---
+++ src/test/java/org/gitPro/test2.java
@@ -0,0 +1,15 @@
+package org.gitPro;
+
+public class test2 {
+public static void main(String[] args) {
+	String s="hungry";
+	if(s=="hungry") {
+		System.out.println("give him burger");
+	}
+	else {
+		System.out.println("give him dosa");
+	}
+
+
+}
+}
